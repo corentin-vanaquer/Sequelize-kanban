ae717c5e91ed5f47344c51d03a5f2ca61db1e802
Corentin <corentin.vnqr@gmail.com>
2022-11-24 17:55:12 +0100

    fixe(router): comments

--- app/router-facto.js
+++ app/router-facto.js
@@ -5,33 +5,6 @@ const router = express.Router();
 const genericController = require('./controllers-facto/genericController');
 const assocController = require('./controllers-facto/assocController');
 
-/*
-pour factoriser les try...catch, on définit une fonction qui va ... retourner une fonction !!
-WHAAAT ? C'est possible ça ? Mais oui, c'est possible, JS c'est magique :-)
-
-Au lieu d'appeler notre méthode de contrôleur comme ça :
-
-router.get('/maroute', monControleur.maMethode)
-
-on va l'appeler comme ça :
-
-router.get('/maroute', async function(request, response, next) {
-    try {
-        await monControleur.maMethode(request, response, next)
-    } catch(error) {
-        console.error(error);
-        response.status(500).json({error: error.message});
-    }
-});
-
-On encapsule l'appel à la méthode du contrôleur dans une fonction qui va gérer le try ... catch
-Pour éviter d'avoir à mettre le code ci-dessus (un peu verbeux) pour toutes nos routes, on crée cette fonction encapsuleuse dans une fonction qui va la retourner, 
-cette fonction prend en paramètre la méthode du contrôleur à exécuter
-On appelle la méthode du contrôleur dans le try...catch de la fonction encapsuleuse, toute erreur dans les méthodes de contrôleur sera capté par ce try...catch, on peut les enlever des méthodes des contrôleurs
-on a factorisé la mise en place du try ... catch pour toutes les méthodes des contrôleurs :-))
-
-*/
-
 
 const genericTryCatch = controllerMethod => {
     return async (request, response, next) => {
